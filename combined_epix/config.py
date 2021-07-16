9840373ea1f1c8e1d5c8494e8b88cfe910b60a5a
Vincent Esposito <espov@slac.stanford.edu>
2021-07-16 15:05:03 -0700

    running averages

--- combined_epix/config.py
+++ combined_epix/config.py
@@ -5,15 +5,15 @@ import numpy as np
 EXP = 'xpplw3319'
 HUTCH = EXP[:3]
 CALIBDIR = ''.join(['/cds/data/psdm/', HUTCH, '/', EXP, '/calib/'])
-LIVE = True
+LIVE = False
 UPDATERATE_CLIENT = 10 # how many shots to process before pushing to master
 UPDATERATE = 2 # visualization update (in number of client pushes)
 LOGLVL = logging.DEBUG
 TOPIC = 'COMB_EPIX'
 
 # Ana config
-PHOT = False # photon plot
-ACCUMULATE = True
-THRES = -100 # for thresholding the image
-PHOTON_ADU = 173.
+PHOT = True # photon plot
+RUNNING_AVERAGE = 10 # In mulitple of UPDATERATE_CLIENT. Set to 0 for infinite average
+THRES = 40 # Pixel threshold
+PHOTON_ADU = 173. # for photonizing
 PIX = 50 # um
--- combined_epix/server.py
+++ combined_epix/server.py
@@ -43,7 +43,13 @@ class Plotter:
         self.sum_img = 0
         self.sum_imgThres = 0
         self.sum_imgPhot = 0
+        
+        if config.RUNNING_AVERAGE:
+            self.ravg = utils.MovingAverage(config.RUNNING_AVERAGE, factor=config.UPDATERATE_CLIENT)
+            self.ravg_phot = utils.MovingAverage(config.RUNNING_AVERAGE, factor=config.UPDATERATE_CLIENT)
+        return
 
+            
     def update(self,md):
         self.nupdate+=1
     
@@ -53,10 +59,6 @@ class Plotter:
             self.imgPhot = md.imgPhot
         except:
             self.imgPhot = None
-        # sums since last update - this is not the best way as each rank keeps a few events only, so it'll be noisier. 
-        # Otherwise we need to send  lot of data though
-        self.running_sumimg = md.sum_img
-        self.running_sumimgThres = md.sum_imgThres
         
         # sums since running
         self.nevents += md.small.nevents
@@ -64,37 +66,55 @@ class Plotter:
         self.sum_imgThres += md.sum_imgThres
         if self.imgPhot is not None:
             self.sum_imgPhot += md.sum_imgPhot
+            
+        # running averages
+        if config.RUNNING_AVERAGE:
+            self.ravg.update_ravg(self.imgThres)
+            self.avg_im = self.ravg.ravg
+            if self.imgPhot is not None:
+                self.ravg_phot.update_ravg(self.imgPhot)
+                self.avg_phot = self.ravg_phot.ravg
+        else:
+            self.avg_im = self.sum_imgThres/self.nevents
+            if self.imgPhot is not None:
+                self.avg_phot = self.sum_imgPhot/self.nevents
         
-        #update
+        # push plots
         if self.nupdate%config.UPDATERATE==0:
-            if self.lasttime is not None:
-                print('Client updates {}, Server received {} events, Rate {}'.format(
-                    self.nupdate, self.nevents, (self.nevents-self.lastnevents)/(time.time()-self.lasttime)))
-            self.lasttime = time.time()
-            self.lastnevents = self.nevents
+            self.update_plots()
+        return
+    
+    
+    def update_plots(self):
+        if self.lasttime is not None:
+            print('Client updates {}, Server received {} events, Rate {}'.format(
+                self.nupdate, self.nevents, (self.nevents-self.lastnevents)/(time.time()-self.lasttime)))
+        self.lasttime = time.time()
+        self.lastnevents = self.nevents
             
-            # Plot 1:
-            imgThres = Image(self.nupdate, "Current image (thres)", md.imgThres, 
+        # Plot 1:
+        imgThres = Image(self.nupdate, "Current image (thres)", self.imgThres, 
+                         xlabel= 'horizontal (pixel)', ylabel= 'vertical (pixel)', aspect_ratio=1)
+        imgThres_sum = Image(self.nupdate, "Average image (thres)", self.avg_im, 
+                         xlabel= 'horizontal (pixel)', ylabel= 'vertical (pixel)', aspect_ratio=1)
+        p1 = MultiPlot(self.nupdate, 'Image', ncols=2)
+        p1.add(imgThres)
+        p1.add(imgThres_sum)
+            
+        # Plot 2:
+        if self.imgPhot is not None:
+            phot = Image(self.nupdate, "Photons", self.imgPhot, 
                              xlabel= 'horizontal (pixel)', ylabel= 'vertical (pixel)', aspect_ratio=1)
-            imgThres_sum = Image(self.nupdate, "Average image (thres)", self.sum_imgThres/self.nevents, 
+            phot_sum = Image(self.nupdate, "Average photons", self.avg_phot, 
                              xlabel= 'horizontal (pixel)', ylabel= 'vertical (pixel)', aspect_ratio=1)
-            p1 = MultiPlot(self.nupdate, 'Image', ncols=2)
-            p1.add(imgThres)
-            p1.add(imgThres_sum)
-            
-            # Plot 2:
-            if self.imgPhot is not None:
-                phot = Image(self.nupdate, "Photons", self.imgPhot, 
-                                 xlabel= 'horizontal (pixel)', ylabel= 'vertical (pixel)', aspect_ratio=1)
-                phot_sum = Image(self.nupdate, "Average photons", self.sum_imgPhot/self.nevents, 
-                                 xlabel= 'horizontal (pixel)', ylabel= 'vertical (pixel)', aspect_ratio=1)
-                p2 = MultiPlot(self.nupdate, 'Photon', ncols=2)
-                p2.add(phot)
-                p2.add(phot_sum)
-            
-            publish.send(config.TOPIC, p1)
-            if self.imgPhot is not None:
-                publish.send(config.TOPIC+'_PHOT', p2)
+            p2 = MultiPlot(self.nupdate, 'Photon', ncols=2)
+            p2.add(phot)
+            p2.add(phot_sum)
+        
+        publish.send(config.TOPIC, p1)
+        if self.imgPhot is not None:
+            publish.send(config.TOPIC+'_PHOT', p2)
+        return
 
 
 
